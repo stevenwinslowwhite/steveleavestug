9c3d4d71b6a099369f63410d0f8d118f135ab894
Steven White <steven@Stevens-MacBook-Pro.local>
2016-04-10 19:49:09 -0500

    V1

--- node-express/routes/index.js
+++ node-express/routes/index.js
@@ -6,19 +6,34 @@ var app = express();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-	var entries = [];
-	db.query('SELECT * FROM blog_entries',function(err,rows){
-	  if(err) throw err;
-      rows.forEach(function(row) {
-      	entries.push({
-      		subject: row.subject,
-      		content: row.entry_text,
-      		date: row.entry_date,
-                  id: row.id
-      	});
+      var entry = null;
+      var elements = [];
+      var subjects = [];
+
+      var entry_id = 1;
+      db.query('SELECT * from blog_entries where id = ' + entry_id, function(err,rows) {
+            if(err) throw err;
+            rows.forEach(function(row) {
+                  db.query('SELECT * from blog_entry_entries where blog_entry_id = ' + entry_id + 
+                        ' order by entry_number asc', function(err, entry_rows) {
+                              if(err) throw err;
+                              entry_rows.forEach(function(entry_row) {
+                                    elements.push({
+                                      content: entry_row.entry_content,
+                                      content_type: entry_row.entry_type
+                                    });
+                              });
+                              entry = {
+                                subject: row.subject,
+                                date: row.entry_date,
+                                content: elements,
+                                id: row.id
+                              };
+                              res.render('index', { entry: entry, subjects: subjects });
+                  });
+                  subjects.push(row.short_subject);
+            });
       });
-	  res.render('index', { entries: entries });
-	});
 });
 
 module.exports = router;
