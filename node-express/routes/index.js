26b2dcfbf9a4685329767a5e711c0b527b1cd430
Steven White <steven@Stevens-MacBook-Pro.local>
2016-04-10 22:24:04 -0500

    V0

--- node-express/routes/index.js
+++ node-express/routes/index.js
@@ -9,34 +9,45 @@ router.get('/', function(req, res, next) {
       var entry = null;
       var elements = [];
       var subjects = [];
-
       db.query('SELECT MAX(ID) as ID from blog_entries', function(err, rows) {
         var rowId = rows[0].ID;
-        console.log("rowId: " + rowId);
+        if (!isNaN(parseFloat(req.query.entry)) && isFinite(req.query.entry) && req.query.entry <= rowId
+            && req.query.entry >= 0) {
+          rowId = req.query.entry;
+        }
         db.query('SELECT * from blog_entries where id = ' + rowId, function(err,rows) {
-              if(err) throw err;
-              rows.forEach(function(row) {
-                    db.query('SELECT * from blog_entry_entries where blog_entry_id = ' + rowId + 
-                           ' order by entry_number asc', function(err, entry_rows) {
-                                if(err) throw err;
-                                entry_rows.forEach(function(entry_row) {
-                                      elements.push({
-                                        content: entry_row.entry_content,
-                                        content_type: entry_row.entry_type
-                                      });
+            var blog_entry = rows[0];
+            if (!blog_entry) {
+              return;
+            }
+            if(err) throw err;
+            db.query('SELECT * from blog_entry_entries where blog_entry_id = ' + rowId + 
+                         ' order by entry_number asc', function(err, entry_rows) {
+                              if(err) throw err;
+                              entry_rows.forEach(function(entry_row) {
+                                    elements.push({
+                                      content: entry_row.entry_content,
+                                      content_type: entry_row.entry_type
+                                    });
+                              });
+                              entry = {
+                                subject: blog_entry.subject,
+                                date: blog_entry.entry_date,
+                                content: elements,
+                                id: blog_entry.id
+                              };
+                              db.query('SELECT short_subject, id from blog_entries', function(err, rows) {
+                                rows.forEach(function(subject) {
+                                  subjects.push({
+                                    id: subject.id,
+                                    name: subject.short_subject
+                                  });
                                 });
-                                entry = {
-                                  subject: row.subject,
-                                  date: row.entry_date,
-                                  content: elements,
-                                  id: row.id
-                                };
                                 res.render('index', { entry: entry, subjects: subjects });
-                    });
-                    subjects.push(row.short_subject);
-              });
+                              });
+                  });
+            });
         });
-      });
 });
 
 module.exports = router;
