01d3fa4861a79c3fa567b7678ad260ed24c2f27a
Steven White <steven@Stevens-MacBook-Pro.local>
2016-04-10 21:32:40 -0500

    few updates

--- node-express/routes/index.js
+++ node-express/routes/index.js
@@ -10,29 +10,32 @@ router.get('/', function(req, res, next) {
       var elements = [];
       var subjects = [];
 
-      var entry_id = 1;
-      db.query('SELECT * from blog_entries where id = ' + entry_id, function(err,rows) {
-            if(err) throw err;
-            rows.forEach(function(row) {
-                  db.query('SELECT * from blog_entry_entries where blog_entry_id = ' + entry_id + 
-                        ' order by entry_number asc', function(err, entry_rows) {
-                              if(err) throw err;
-                              entry_rows.forEach(function(entry_row) {
-                                    elements.push({
-                                      content: entry_row.entry_content,
-                                      content_type: entry_row.entry_type
-                                    });
-                              });
-                              entry = {
-                                subject: row.subject,
-                                date: row.entry_date,
-                                content: elements,
-                                id: row.id
-                              };
-                              res.render('index', { entry: entry, subjects: subjects });
-                  });
-                  subjects.push(row.short_subject);
-            });
+      db.query('SELECT MAX(ID) as ID from blog_entries', function(err, rows) {
+        var rowId = rows[0].ID;
+        console.log("rowId: " + rowId);
+        db.query('SELECT * from blog_entries where id = ' + rowId, function(err,rows) {
+              if(err) throw err;
+              rows.forEach(function(row) {
+                    db.query('SELECT * from blog_entry_entries where blog_entry_id = ' + rowId + 
+                           ' order by entry_number asc', function(err, entry_rows) {
+                                if(err) throw err;
+                                entry_rows.forEach(function(entry_row) {
+                                      elements.push({
+                                        content: entry_row.entry_content,
+                                        content_type: entry_row.entry_type
+                                      });
+                                });
+                                entry = {
+                                  subject: row.subject,
+                                  date: row.entry_date,
+                                  content: elements,
+                                  id: row.id
+                                };
+                                res.render('index', { entry: entry, subjects: subjects });
+                    });
+                    subjects.push(row.short_subject);
+              });
+        });
       });
 });
 
