f5ae93ec7f4c9f512a9176a546ceaffd0b9e30b9
Steven White <steven@Stevens-MacBook-Pro.local>
2016-04-16 08:52:09 +0530

    adding scrollable nav

---
+++ node-express/public/javascripts/entry_nav.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+	var numElements = document.getElementById('scroller').children.length;
+	var multiplier = 190;
+	if (window.innerWidth <= 1000) {
+		multiplier = 290;
+	}
+	document.getElementById('scroller').setAttribute('style', 'width: ' + ((numElements-1) * multiplier) + 'px');
+
+	// var queryParam = window.location.search;
+	// var entryId = numElements - 1;
+	// if (queryParam.indexOf('entry=') > -1) {
+	//   entryId = queryParam.substring(queryParam.indexOf("entry=") + 6);
+	// }
+	// var container = document.getElementById('scroller');
+	// var rowToScrollTo = document.getElementById('entry-' + entryId);
+	// container.scrollTop = rowToScrollTo.offsetTop;
+}
