25eef5222cc7ecde364a3f16723157625d0baf25
Steven White <stevenwinslowwhite@gmail.com>
2016-10-15 12:44:30 -0700

    Removing comments

--- node-express/routes/comments.js
+++ node-express/routes/comments.js
@@ -15,26 +15,6 @@ router.get('/', function(req, res, next) {
     }
 });
 
-router.post('/', function(req, res) {
-	var name = req.body.name;
-	if (name == "Steven" || name == "steven"
-		|| name == "Steve" || name == "steve") {
-		name = "Somebody who tried to use Steven as their name";
-	} else if (name === "mypass") {
-		name = "Steven";
-	}
-	var comment = req.body.comment.substring(0, 1000);
-	if (comment.length == 1000) {
-		comment += " (Truncated remaining comments, max 1000 chars)";
-	}
-	var comment = { name: name, comment: comment,
-		entry_date: new Date(), entry_reference: req.body.entry_reference };
-
-	db.query('INSERT INTO comments SET ?', comment, function(err, result) {
-		console.log("inserted");
-	});
-	redirect(req, res);
-});
 
 function redirect(req, res) {
 	var url = req.header("Referer") + "#comments";
